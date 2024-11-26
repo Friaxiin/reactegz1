c8ce3f95ff17328f3171c36716dd19aa739c4485
patryk.labuz <patryk.labuz@S50-11>
2024-11-26 10:09:49 +0100

    Dodaj pliki projektów.

---
+++ reactegz1/Film.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactegz1
+{
+    /****************************************
+     * nazwa klasy:     Film
+     * pola:            tytul - przechowuje tytul filmu
+     *                  wypozyczenia - przechowuje liczbe wypozyczen filmu
+     * metody:          getWypozyczenia, zwraca liczbe wypozyczen, metoda pozwala na zwrocenie liczby wypozyczen filmu
+     *                  getTytul, zwraca tytul filmu - metoda pozwala na zwrocenie tytulu filmu
+     *                  setTytul, brak - metoda pozwala na ustawienie tytulu filmu
+     *                  inkrementacja, zwraca liczbe wypozyczen filmu powiekszone o 1 - pozwala na zwiekszenie liczby wypozyczen
+     * informacje:      klasa reprezentuje obiekt filmu zawierajacego tytul oraz liczbe jego wypozyczen
+     * autor:           PESEL Patryk Łabuz
+     * 
+    ****************************************/
+    public class Film
+    {
+        protected string tytul = "";
+        protected int wypozyczenia = 0;
+
+        public int getWypozyczenia()
+        {
+            return wypozyczenia;
+        }
+        public string getTytul()
+        {
+            return tytul;
+        }
+        public void setTytul(string tytul)
+        {
+            this.tytul = tytul;
+        }
+        public int inkrementacja()
+        {
+            return wypozyczenia++;
+        }
+    }
+}
---
+++ reactegz1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace reactegz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Film film = new Film();
+
+            Console.WriteLine(film.getTytul());
+            Console.WriteLine(film.getWypozyczenia());
+
+            Console.WriteLine();
+
+            film.setTytul("Sami swoi");
+
+            Console.WriteLine(film.getTytul());
+
+            Console.WriteLine("Wypożyczenia: " + film.getWypozyczenia());
+
+            film.inkrementacja();
+
+            Console.WriteLine("Wypożyczenia po inkrementacji: " + film.getWypozyczenia());
+        }
+    }
+}
